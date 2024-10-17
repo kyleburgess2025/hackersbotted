501284bf0694985ce63c6a8ad56e4f165c20487a
kyleburgess <kylie.m.burgess@vanderbilt.edu>
2024-10-17 16:15:03 -0500

    Changing cronjob

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,8 @@ module.exports = {
   name: Events.ClientReady,
   once: true,
   async execute(client) {
-    let scheduledMessage = new cron.CronJob("14 16 * * *", async () => {
+    let scheduledMessage = new cron.CronJob("16 16 * * *", async () => {
+      console.log("hello")
       let channel = client.channels.cache.get(process.env.CHANNEL_ID);
       channel.send(await createBonus());
     });
